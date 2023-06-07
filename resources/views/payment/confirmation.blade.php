fa9e8c4f838344051ae08394b53c33c73ebe4cd2
Andreas Strömgren <andreas@stromgren.nu>
2023-06-07 08:48:35 +0200

    Updates

--- resources/views/payment/confirmation.blade.php
+++ resources/views/payment/confirmation.blade.php
@@ -2,9 +2,18 @@
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         @csrf
         <div class="text-gray-900">
-            <h1 class="text-3xl mb-5">Bekräftelse registrering {{ $competitor->competitionYear->name }}</h1>
+            <h1 class="text-3xl mb-5">Kvitto registrering {{ $competitor->competitionYear->name }}</h1>
             <p>Din bokning är nu betalt och du är anmäld till {{ $competitor->competitionYear->name }}.</p>
             <p class="mb-5">Ett bekräftelsemail har skickats till {{ $competitor->email }}.</p>
+            <div class="mb-5">
+                <p>Din anmälan har referensnummer: <b>{{ $competitor->id }}</b></p>
+                @if($reference_number = rescue(fn() => array_slice(explode('/', $competitor->getPaymentData('current-payment-paid')['paymentOrder']['id']), -1)[0], null, false))
+                    <p>Din betalning har referensnummer: <b>{{ $reference_number }}</b></p>
+                @endif
+            </div>
+            <div class="mb-5">
+                <p>Om du har frågor angående din anmälan eller betalning så kan du kontakta oss på <a href="mailto:kkjoggen@gmail.com" class="text-green-500 underline">kkjoggen@gmail.com</a>.</p>
+            </div>
             @include('partials.payer', ['competitor' => $competitor])
             @include('partials.children', ['competitor' => $competitor])
         </div>
